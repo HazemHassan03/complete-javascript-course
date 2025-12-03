2e1b1e833ae76421d5ea32c683f88432e0cc1835
HazemHassan03 <hazemhassan0321@gmail.com>
2025-12-03 18:29:32 +0200

    assignment_4

--- assignments/assignments.js
+++ assignments/assignments.js
@@ -22,6 +22,7 @@ console.log(typeof country);
 console.log(typeof language);
 */
 
+/*
 // Assignment 3 - let, const and var
 const country = "Egypt";
 const continent = "Africa";
@@ -33,3 +34,23 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+*/
+
+// Assignment 4 - Basic Operators
+const country = "Egypt";
+const continent = "Africa";
+let population = 119;
+const isIsland = false;
+const language = "Arabic";
+
+console.log(population / 2);
+
+population++;
+console.log(population);
+
+console.log(population > 6);
+
+console.log(population < 33);
+
+const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+console.log(description);
