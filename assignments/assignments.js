4ae5fd669029e79055d9259614782dd07f6b09c0
HazemHassan03 <hazemhassan0321@gmail.com>
2025-12-03 18:19:10 +0200

    assignment_2

--- assignments/assignments.js
+++ assignments/assignments.js
@@ -1,3 +1,5 @@
+/*
+// Assignment 1 - Values and Variables
 let country = "Egypt";
 let continent = "Africa";
 let population = 119;
@@ -5,3 +7,15 @@ let population = 119;
 console.log(country);
 console.log(continent);
 console.log(population);
+*/
+
+// Assignment 2 - Data Types
+let country = "Egypt";
+let continent = "Africa";
+let population = 119;
+const isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
