f895d4f0ba47d027fa7034fde47af2855d92cbbf
HazemHassan03 <hazemhassan0321@gmail.com>
2025-12-03 18:32:29 +0200

    assignment_5

--- assignments/assignments.js
+++ assignments/assignments.js
@@ -36,6 +36,7 @@ console.log(typeof country);
 console.log(typeof language);
 */
 
+/*
 // Assignment 4 - Basic Operators
 const country = "Egypt";
 const continent = "Africa";
@@ -53,4 +54,24 @@ console.log(population > 6);
 console.log(population < 33);
 
 const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+console.log(description);
+*/
+
+// Assignment 5 - Strings and Template Literals
+const country = "Egypt";
+const continent = "Africa";
+let population = 119;
+const isIsland = false;
+const language = "Arabic";
+
+console.log(population / 2);
+
+population++;
+console.log(population);
+
+console.log(population > 6);
+
+console.log(population < 33);
+
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 console.log(description);
